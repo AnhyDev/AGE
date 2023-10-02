bf2c6501b63a08a305539b6f7db1db70ec6bfcf6
korvinius <korvinius@Anhydrite>
2023-10-02 05:33:28 +0300

    Edit UtilityAnh

--- solidity/global/anhydrite/UtilityAnh.sol
+++ solidity/global/anhydrite/UtilityAnh.sol
@@ -55,7 +55,7 @@ abstract contract UtilityAnh is BaseUtility {
 
     // Returns the Ethereum address of the proxy contract.
     function getProxyAddress() public view returns (address) {
-        return getProxyAddress();
+        return _getProxyAddress();
     }
 
     /*
