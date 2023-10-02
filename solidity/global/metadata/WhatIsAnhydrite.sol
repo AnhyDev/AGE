6d70d052a143c2960ee6d3163c5272b9c1df89b2
korvinius <korvinius@Anhydrite>
2023-10-02 05:30:14 +0300

    Minor edits

---
+++ solidity/global/metadata/WhatIsAnhydrite.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract WhatIsAnhydrite {
+    
+    function whatIsAnhydrite() public pure returns (string[] memory) {
+        string[] memory anhydriteDescription = new string[](14);
+        
+        anhydriteDescription[0] = "The Significance of Anhydrite in Our Project";
+        anhydriteDescription[1] = "Anhydrite is more than just a mineral; it represents strength, unity, and transformation. "
+                                    "Its unique qualities enable it to combine different elements into a cohesive whole, "
+                                    "similar to our platform's goal of merging the worlds of gaming and blockchain.";
+        
+        anhydriteDescription[2] = "Strength Through Unity";
+        anhydriteDescription[3] = "Anhydrite is often used as a 'binding agent' to connect diverse materials. "
+                                   "In our case, the Anhydrite Gaming Ecosystem (AGE) acts as a digital 'cement,' "
+                                   "bringing together gamers, server administrators, and developers into a single ecosystem "
+                                   "rooted in blockchain technology.";
+        
+        anhydriteDescription[4] = "Transformation and Evolution";
+        anhydriteDescription[5] = "Anhydrite is formed through the evaporation of water from gypsum, which is similar to evolution "
+                                    "and adaptation. Our platform goes beyond just integrating blockchain into gaming; it creates "
+                                    "new opportunities for transforming these industries and elevating them to new levels of development.";
+        
+        anhydriteDescription[6] = "Endless Possibilities";
+        anhydriteDescription[7] = "Just as anhydrite can be used in various industrial processes, our project offers numerous opportunities "
+                                   "for growth and adaptation, from simple cryptocurrency exchanges to the creation of unique NFTs and "
+                                   "integration with other blockchain projects.";
+        
+        anhydriteDescription[8] = "In Conclusion";
+        anhydriteDescription[9] = "In conclusion, Anhydrite is not just a name; it's our philosophy. We believe in the power of unity "
+                                   "and transformation, and our aim is not just to create a technological platform, but a unified ecosystem "
+                                   "that intertwines gaming and blockchain for the benefit of the entire community.";
+        
+        return anhydriteDescription;
+    }
+}
